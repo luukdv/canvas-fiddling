f4bfbae9af687221c12b5309703dc70f021c0274
Luuk de Vlieger <info@luuk.site>
2018-11-27 16:58:18 +0100

    :construction: Draw cube

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+import { BoxGeometry, Mesh, MeshNormalMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three'
+
+const camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10)
 const canvas = document.querySelector('canvas')
-const setDimensions = () => {
-  canvas.height = window.innerHeight
-  canvas.width = window.innerWidth
-}
+const geometry = new BoxGeometry(0.2, 0.2, 0.2)
+const material = new MeshNormalMaterial()
+const mesh = new Mesh(geometry, material)
+const renderer = new WebGLRenderer()
+const scene = new Scene()
+
+const setDimensions = () => renderer.setSize(window.innerWidth, window.innerHeight)
 const resize = () => {
   let debounced
 
@@ -17,3 +23,9 @@ const resize = () => {
 
 setDimensions()
 window.addEventListener('resize', resize())
+
+camera.position.z = 1
+scene.add(mesh)
+renderer.render(scene, camera)
+
+document.body.appendChild(renderer.domElement)
