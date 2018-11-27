b6af4e9281fe8942b1ab934dab25152e79fb6707
Luuk de Vlieger <info@luuk.site>
2018-11-27 17:09:33 +0100

    :sparkles: Set animation loop

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { BoxGeometry, Mesh, MeshNormalMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three'
 
-const camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10)
+const camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight)
 const canvas = document.querySelector('canvas')
 const geometry = new BoxGeometry(0.2, 0.2, 0.2)
 const material = new MeshNormalMaterial()
@@ -8,6 +8,7 @@ const mesh = new Mesh(geometry, material)
 const renderer = new WebGLRenderer({ antialias: true })
 const scene = new Scene()
 
+const draw = () => renderer.render(scene, camera)
 const setDimensions = () => renderer.setSize(window.innerWidth, window.innerHeight)
 const resize = () => {
   let debounced
@@ -26,6 +27,8 @@ window.addEventListener('resize', resize())
 
 camera.position.z = 1
 scene.add(mesh)
-renderer.render(scene, camera)
+
+draw()
+renderer.setAnimationLoop(draw)
 
 document.body.appendChild(renderer.domElement)
