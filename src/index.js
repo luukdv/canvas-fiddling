fc34478d488a7297504f0edccd23fc62ab17ac25
Luuk de Vlieger <info@luuk.site>
2018-11-27 16:58:52 +0100

    :art: Antialiasing

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const canvas = document.querySelector('canvas')
 const geometry = new BoxGeometry(0.2, 0.2, 0.2)
 const material = new MeshNormalMaterial()
 const mesh = new Mesh(geometry, material)
-const renderer = new WebGLRenderer()
+const renderer = new WebGLRenderer({ antialias: true })
 const scene = new Scene()
 
 const setDimensions = () => renderer.setSize(window.innerWidth, window.innerHeight)
